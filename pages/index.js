dfe47b4e903aafe3013e7667df83b6e85489a8c9
Olga <belkololol@gmail.com>
2021-07-09 16:32:53 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from "react";
+import {Grid, makeStyles} from "@material-ui/core";
+
+const styles = makeStyles({
+    root: {
+        color: 'red',
+    },
+});
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDynamic: true
+        }
+        this.toggleDynamic = this.toggleDynamic.bind(this);
+    }
+
+    toggleDynamic() {
+        this.setState({
+            isDynamic: !this.state.isDynamic
+        })
+    }
+
+    render() {
+        const data = {
+            id: "809",
+            options: {
+                params: {
+                    fields: {
+                        isDynamic: true
+                    }
+                }
+            },
+            count: 6,
+            color: "#2c2a2a",
+            data: "01.01.2021"
+        }
+        return (
+            <div>
+                <Page data={data} isDynamic={this.state.isDynamic}/>
+                <button onClick={this.toggleDynamic}>
+                    Click
+                </button>
+            </div>
+
+        )
+    }
+
+}
+
+function Page(props) {
+    return <MyWonderfulComponent data={props.data} isDynamic={props.isDynamic}>
+        I'm text from a component
+    </MyWonderfulComponent>
+}
+
+function MyWonderfulComponent(props) {
+    const classes = styles();
+    const {count, options, id} = props.data;
+    const [sum, setSum] = useState(count);
+    useEffect(() => {
+        if (id && options?.params?.fields?.isDynamic) {
+            setSum(sum + 1);
+        }
+    }, [props.isDynamic]);
+    return (
+        <div>
+            <h1 className={classes.root}>Hello World!</h1>
+            <Grid>
+                <Grid item xs={12}>{props.children}</Grid>
+                <Grid>{sum}</Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default Home;
+
